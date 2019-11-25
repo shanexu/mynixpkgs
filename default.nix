aa954aa52bf4778028fc4f669c62466f03ab037b
Xu Shane <xusheng0711@gmail.com>
2019-11-25 10:44:33 +0800

    add nix

--- default.nix
+++ default.nix
@@ -3,11 +3,13 @@ let
     allowUnfree = true;
     packageOverrides = pkgs: rec {
       darwin = pkgs.darwin // mydarwin;
+      jenv = with pkgs; import ./tools/misc/jenv { inherit stdenv fetchurl makeWrapper; };
     };
   };
-  mydarwin = import ./darwin { inherit pkgs; };
-  pkgs = import <nixpkgs> { inherit config; };
 
+  mydarwin = import ./darwin { inherit pkgs; };
+
+  pkgs = import <nixpkgs> { inherit config; };
 in
 pkgs
 
