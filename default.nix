52f1a90c43bca6be0e345414a6ef763e3963cf37
Xu Shane <xusheng0711@gmail.com>
2019-11-30 14:38:42 +0800

    coreutils

--- default.nix
+++ default.nix
@@ -3,8 +3,10 @@ let
     allowUnfree = true;
     packageOverrides = pkgs: rec {
       darwin = pkgs.darwin // mydarwin;
-      jenv = with pkgs; import ./tools/misc/jenv { inherit stdenv fetchurl makeWrapper; };
-      glibtool = with pkgs; import ./development/tools/misc/glibtool { inherit stdenv fetchurl m4 perl help2man; };
+      jenv = pkgs.callPackage ./tools/misc/jenv { };
+      glibtool = pkgs.callPackage ./development/tools/misc/glibtool { };
+      coreutils = pkgs.callPackage ./tools/misc/coreutils { };
+      coreutils-prefixed-conflict = coreutils.override { withPrefix = true; singleBinary = false; unprefixNoConflict = true; };
     };
   };
 
